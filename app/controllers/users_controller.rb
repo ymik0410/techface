b98e5859184fb5fb6146fde673391850f8bfe570
Yulia Mikhaylova ymik0410@gmail.com <ymik0410@gmail.com>
2021-09-08 13:47:01 +0200

    User model with devise

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  before_action :set_user
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+
+end
