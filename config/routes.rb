a26d922bbdafdf4fc703724aa229c0af84015831
Yulia Mikhaylova <ymik0410@gmail.com>
2022-01-05 16:50:18 +0100

    Edit and update methods for the help, reason and expectations

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
 
-  resources :mentees
+  # resources :mentees
+  get "mentees/:id", to: "mentees#show", as: :mentee
+  get "mentees/new", to: "mentees#new", as: "new_mentee"
+  post "mentees", to: "mentees#create"
+  get "mentees/:id/edit", to: "mentees#edit", as: "edit_mentee"
+  patch "mentees/:id", to: "mentees#update"
+
 end
